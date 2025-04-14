cea46724830658181d3edc5221980ea41ce61fd2
less <ndeurys@gmail.com>
2025-04-14 21:17:34 +0000

    ajout menu mobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,18 +3,52 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule, MatIconModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  animations: [
+    trigger('menuAnimation', [
+      state(
+        'hidden',
+        style({
+          opacity: 0,
+          transform: 'translateY(-10px)',
+          // display: 'none',
+        })
+      ),
+      state(
+        'visible',
+        style({
+          opacity: 1,
+          transform: 'translateY(0)',
+          // display: 'block',
+        })
+      ),
+      transition('hidden <=> visible', [animate('300ms ease-in-out')]),
+    ]),
+  ],
 })
 export class HeaderComponent {
+  menuMobile: boolean = false;
   navItems = [
     { name: 'Home', path: '/home' },
     { name: 'Blog', path: '/blog' },
     { name: 'About', path: '/about' },
     { name: 'Newsletter', path: '/newsletter' },
   ];
+
+  toggleMenuMobile() {
+    this.menuMobile = !this.menuMobile;
+  }
 }
